052ea2ce3a785b20778f3ba86fb324fab60e06ba
TomoCoder123 <thomasxdeng@gmail.com>
2024-06-25 20:24:36 -0700

    Log throttle accleration

--- scripts/car_test.py
+++ scripts/car_test.py
@@ -21,6 +21,7 @@ import argparse
 
 from airsim_data_collection.sensors.sensor_handler import SensorHandler
 from airsim_data_collection.utility.convert_pose import quaternion_to_eul, angle_diff, airsimpos2np
+import matplotlib.pyplot as plt
 
 
 
@@ -51,17 +52,55 @@ if __name__ == "__main__":
     car_state = client.getCarState()
     print("car state: %s" % car_state)
 
+    acceleration_list = np.zeros((10,10,3))
+    velocity_list = np.zeros((10,10,3))
+    throttle_values = []
 
     try:
         print("driving routes")
-        while(True):
+        for i, throttle in enumerate(np.linspace(0.1, 1.0, 10)):
+            print("executing throttle: " + str(throttle))
             car_controls.steering = 0.0  #0.2 * np.sin(0.01 * count)
-            car_controls.throttle = 1.0
+            car_controls.throttle = throttle
             client.setCarControls(car_controls)
-            time.sleep(1)
+            car_controls.brake = 0  
+            #time.sleep(10)
+            throttle_values.append(throttle)
+            for j in range(10):
+                
+                car_state = client.getCarState()
+                x_acc =  car_state.kinematics_estimated.linear_acceleration.x_val
+                y_acc =  car_state.kinematics_estimated.linear_acceleration.y_val
+                z_acc =  car_state.kinematics_estimated.linear_acceleration.z_val
+                acceleration_list[i,j,:] =  np.array([x_acc, y_acc, z_acc])
 
-            car_state = client.getCarState()
-            print("car state: %s" % car_state)
+                x_vel = car_state.kinematics_estimated.linear_velocity.x_val
+                y_vel = car_state.kinematics_estimated.linear_velocity.y_val
+                z_vel = car_state.kinematics_estimated.linear_velocity.z_val
+                velocity_list[i,j,:] =  np.array([x_vel, y_vel, z_vel])
+
+                velocity = np.array([x_vel, y_vel, z_vel])
+
+                print("acceleration")
+                print(np.array([x_acc, y_acc, z_acc]))
+                # print("velocity")
+
+                # print(np.array([x_vel, y_vel, z_vel]))
+
+
+                time.sleep(1)
+
+            #print("car state: %s" % car_state)
+        acceleration_norm = np.linalg.norm(acceleration_list, axis=2)
+        acceleration_norm_avg = np.mean(acceleration_norm, axis = 1)
+        plt.figure()
+        plt.plot(throttle_values, acceleration_norm_avg, marker = 'o')
+        plt.title("Acceleration vs Throttle")
+        plt.xlabel("Throttle")
+        plt.ylabel("Acceleration")
+        plt.grid(True)
+        plt.show()
+
 
 
     except KeyboardInterrupt:
--- scripts/state_logger.py
+++ scripts/state_logger.py
@@ -6,26 +6,46 @@ import numpy as np
 import airsim
 import time
 
-client = airsim.MultirotorClient()
+client = airsim.CarClient()
 client.confirmConnection()
 
 dt = 0.1
 states = []
+accelerations = []
 collision_count = 0
+velocities = []
 
 try:
     while True:
         pose = client.simGetVehiclePose()
+        car_state = client.getCarState()
+        #print(car_state)
         x, y, z = pose.position.x_val, pose.position.y_val, pose.position.z_val
-        position = np.array([x, y, z])
-        states.append(position)
-        print(f"x = {x:.2f}, y = {y:.2f}, z = {z:.2f}")
+        x_acc =  car_state.kinematics_estimated.linear_acceleration.x_val
+        y_acc =  car_state.kinematics_estimated.linear_acceleration.y_val
+        z_acc =  car_state.kinematics_estimated.linear_acceleration.z_val
+        acceleration =  np.array([x_acc, y_acc, z_acc])
+
+        x_vel = car_state.kinematics_estimated.linear_velocity.x_val
+        y_vel = car_state.kinematics_estimated.linear_velocity.y_val
+        z_vel = car_state.kinematics_estimated.linear_velocity.z_val
+        velocity = np.array([x_vel, y_vel, z_vel])
+
+        #position = np.array([x, y, z])
+        #states.append(position)
+        accelerations.append(acceleration)
+        #print(f"x = {x:.2f}, y = {y:.2f}, z = {z:.2f}")
+        #print(f"x_acc = {x_acc:.2f}, y_acc = {y_acc:.2f}, z_acc = {z_acc:.2f}")
+        print(f"x_vel = {x_vel:.6f}, y_vel = {y_vel:.6f}, z_vel = {z_vel:.6f}")
+
         time.sleep(dt)
 
 except KeyboardInterrupt:
     print("Saving data")
     timestamp = time.strftime("%Y%m%d-%H%M")
     np.savez(f'../data/states_{timestamp}.npz', states=states)
+    np.savez(f'../data/accelerations_{timestamp}.npz', accelerations=accelerations)
+
     print("Interrupted by user, shutting down")
     client.enableApiControl(False)
     exit()
